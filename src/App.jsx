64428ae2077e13f01c275d72301f013e8170dfe8
Arnav Kapadia <arnavkapadia@gmail.com>
2026-01-14 01:07:12 -0800

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Food from './Food.jsx'
+import Counter from './Counter.jsx'
+
+function App() {
+  return (
+    <>
+      <Footer />
+      <Header />
+      <Food />
+      <Counter />
+    </>
+  );
+}
+
+export default App
